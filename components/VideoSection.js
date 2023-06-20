9e905ed91d48201ced2ea7450302ea8e60c92336
Harsh <harshkankaria9@gmail.com>
2023-06-20 20:58:21 +0530

    Video aligned

--- components/VideoSection.js
+++ components/VideoSection.js
@@ -1,22 +1,21 @@
 export default function VideoSection({ showVideo }) {
-    return (
-      <div className="relative flex place-items-center w-full">
-        {showVideo ? (
-          // Embedded YouTube video
-          <div className="w-full">
-            <iframe
-              className="w-full h-96 md:h-64"
-              src="https://www.youtube.com/embed/ZByhs9mDtDg"
-              title="Embedded Video"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          </div>
-        ) : (
-          // Video placeholder
-          <div className="w-180 h-37 bg-gray-200">Video Here</div>
-        )}
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      {showVideo ? (
+        // Embedded YouTube video
+        <div className="w-full p-4">
+          <iframe
+            className="w-1/2 md:h-96"
+            src="https://www.youtube.com/embed/ZByhs9mDtDg"
+            title="Embedded Video"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ) : (
+        // Video placeholder
+        <div className="w-180 h-37 bg-gray-200">Video Here</div>
+      )}
+    </div>
+  );
+}
